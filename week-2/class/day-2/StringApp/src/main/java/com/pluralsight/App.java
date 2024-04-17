811a8855aae1a79d277f1136b1d3632cf1540ff0
Chris <chrisbyronc@gmail.com>
2024-04-17 08:25:46 -0400

    mornign day 3 week 2

--- week-2/class/day-2/StringApp/src/main/java/com/pluralsight/App.java
+++ week-2/class/day-2/StringApp/src/main/java/com/pluralsight/App.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.util.regex.Pattern;
+
 public class App {
     public static void main(String[] args) {
         String favoriteAnimal = "turtle,kangaroo,tiger,panda";
@@ -10,6 +12,27 @@ public class App {
         System.out.println(favoriteAnimal.substring(7, 1));
 
         String[] animals = favoriteAnimal.split("\\,");
+
+
+
+        // string contains "id|description|quantity|price"
+
+        String input = "111|Hot Chocolate (12-count)|21|4.99";
+
+
+
+        String[] tokens = input.split(Pattern.quote("|"));
+
+//        tokens[0] = "111"
+//        tokens[1] = "Hot Chocolate (12-count)"
+//        tokens[2] = "21"
+//        tokens[3] = "4.99"
+
+        int id = Integer.parseInt(tokens[0]); //int 111
+        String name = tokens[1];
+        int quantity = Integer.parseInt(tokens[2]); //int 21
+        double price = Double.parseDouble(tokens[3]); //double 4.99
+
 
     }
 }
--- week-2/class/day-2/day-2-objectives.md
+++ week-2/class/day-2/day-2-objectives.md
@@ -16,3 +16,28 @@ Today's Agenda
 8. Break
 9. Classes, getters and setters
 10. Recap and Q&A
+
+
+
+What did you learn today?
+
+1. switch
+2. Java is 0 based index system
+3. String methods
+   1. .trim() - removes whitespace in front and back of string
+   2. .length() - how many characters in string
+   3. .indexOf(char) - index of character
+   4. .charAt(index) - finds character at index
+   5. .split() - splits strings
+   6. .equalsIgnoreCase()
+   7. .toLowerCase()
+   8. .toUpperCase()
+   9. .endsWith()
+   10. .startsWith()
+4. Parse
+   1. Integer.parseInt("9001") - extracts int from string. IE "9000" -> 9000
+   2. Double.parseDouble()
+5. Pattern.quote() - pattern to match
+6. StringBuilder - builds string
+   1. .append() - does the building
+   2. .toString()
